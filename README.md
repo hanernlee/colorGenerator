e3840f9fcdde790a5c3b213ad04a249a5d441417
Christopher Lee <hanernlee@gmail.com>
2016-08-16 20:48:46 +1000

    refactorize RGB var

--- README.md
+++ README.md
@@ -4,13 +4,13 @@
 The app should generate a gradient color based on two hex color inputs.
 
 #Solution
-There are currently two versions of solution to this problem, one using toString() with parseInt() and the other without any of those methods to convert the hexadecimal values.
+There are currently two versions of solution to this problem, one using toString() with parseInt() and the other without any of those methods to convert the hexadecimal values (needs factorising).
 
-Feel free to check out both JS files for the solution. The live app currently runs on app2.js, without toString() and parseInt() methods.
+Feel free to check out both JS files for the solution. The live app currently runs on app.js.
 
 #Features
 You can immediately utilise the app by entering a 6 digit color code without '#', (eg. FFFFFF or 009688).
 
-#### [Test the Color Generator App] (http://hanernlee.github.io/colorGenerator)
+#### [Test the Color Generator App here!] (http://hanernlee.github.io/colorGenerator)
 
 *README TO BE UPDATED FURTHER*
--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,12 @@
 $( document ).ready(function() {
 
-  //converts Hex Code to RGB Color No.
+  //converts Hex Code to RGB Color in an Array
   var hexToRGB = function(hexCode) {
     var hexColor = hexCode.replace('#','');
-    var R = parseInt(hexColor.slice(0,2),16);
-    var G = parseInt(hexColor.slice(2,4),16);
-    var B = parseInt(hexColor.slice(4,6),16);
-    return {R, G, B};
+    //a better approach using .map returns array of 3 RGB values
+    return [0,2,4].map(function(n,i) {
+      return parseInt(hexColor.slice(n,n+2),16);
+    });
   };
 
   //converts RGB back to Hex Code
@@ -20,46 +20,46 @@ $( document ).ready(function() {
     return finalHexColor;
   };
 
+  //loop through to generate each color pallete and storing in an array
+  var generateSteps = function(firstRGB,diffs,steps) {
+    var hexColorArray = [];
+    //cStr is the previous value
+    for (var j = 0; j <= steps; j ++) {
+      hexColorArray.push(
+        firstRGB.reduce(function(cStr,val,i) {
+          return cStr + rgbToHex(val, diffs[i], steps, j);
+        }, ''));
+    };
+    return hexColorArray;
+  };
+
+  //looping through the array to generate DIV element with bgColor and append to HTML
+  var generatePalette = function(hexColorArray) {
+    for (var i = 0; i < hexColorArray.length; i++) {
+      var $paletteDiv = $('<div>').css({
+        backgroundColor: '#'+hexColorArray[i],
+      });
+
+      $paletteDiv.addClass('paletteBox');
+      $('.paletteContainer').append($paletteDiv);
+    }
+  };
+
   $('#generateBtn').on('click', function() {
     $('.paletteContainer').empty();
-    var firstColor = $('#colorOne').val();
-    var secondColor= $('#colorTwo').val();
+    var firstRGB = hexToRGB($('#colorOne').val());
+    var secondRGB = hexToRGB($('#colorTwo').val());
     //reducing steps by 1 to exclude the first step
     var steps = parseInt($('#numSteps').val())-1;
-    var hexColorArray = [];
 
-    var firstRGB = hexToRGB(firstColor);
-    var secondRGB = hexToRGB(secondColor);
+    //returns difference between RGB of both color values in an array
+    var diffs = secondRGB.map(function(cValue,i) {
+      return cValue - firstRGB[i]
+    });
 
-    //difference between RGB of both colors
-    var rDiff = secondRGB.R - firstRGB.R;
-    var gDiff = secondRGB.G - firstRGB.G;
-    var bDiff = secondRGB.B - firstRGB.B;
-
-    //loop through to generate each color pallete and storing in an array
-    var generateSteps = function() {
-      for (var i = 0; i <= steps; i ++) {
-        var newR = rgbToHex(firstRGB.R,rDiff,steps,i);
-        var newG = rgbToHex(firstRGB.G,gDiff,steps,i);
-        var newB = rgbToHex(firstRGB.B,bDiff,steps,i);
-        var newHexColor = newR+''+newG+''+newB;
-        hexColorArray.push(newHexColor);
-      };
-    };
-    generateSteps();
-
-    //looping through the array to generate DIV element with bgColor and append to HTML
-    var generatePalette = function() {
-      for (var i = 0; i < hexColorArray.length; i++) {
-        var $paletteDiv = $('<div>').css({
-          backgroundColor: '#'+hexColorArray[i],
-        });
-
-        $paletteDiv.addClass('paletteBox');
-        $('.paletteContainer').append($paletteDiv);
-      }
-    };
-    generatePalette();
+    generatePalette(
+      generateSteps(firstRGB, diffs, steps)
+    );
   });
 
   //clear palette container and all input fields
